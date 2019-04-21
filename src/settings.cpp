82f4fe25ec5ac93853989b0b86ae8af739736ded
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2019-04-21 04:11:57 +0300

    Implement new settings in chrome57 version

--- src/settings.cpp
+++ src/settings.cpp
@@ -10,34 +10,88 @@
 
 std::string g_applicationSettingsError = "";
 
+inline bool FileExists(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
+}
+
+json_value* GetJson(std::string jsonFileName) {
+	json_value* ret = new json_value();
+
+	std::string jsonFile = GetExecutableDirectory() + "/" + jsonFileName;
+	if (!FileExists(jsonFile)) {
+		g_applicationSettingsError = "Error: file " + jsonFileName + " does not exist.";
+		LOG_WARNING << g_applicationSettingsError;
+		return ret;
+	}
+	LOG_DEBUG << "Fetching json from " << jsonFileName << " file";
+	std::string contents = GetFileContents(jsonFile);
+	if (contents.empty()) {
+		g_applicationSettingsError = "Error while opening the " + jsonFileName + " file.";
+		LOG_WARNING << g_applicationSettingsError;
+		return ret;
+	}
+
+	json_settings settings;
+	memset(&settings, 0, sizeof(json_settings));
+	char error[256];
+	json_value* json_parsed = json_parse_ex(&settings, contents.c_str(), &error[0]);
+	if (json_parsed == 0) {
+		g_applicationSettingsError = "Error while parsing " + jsonFileName + " file: " + error;
+		LOG_WARNING << g_applicationSettingsError;
+		return ret;
+	}
+	ret = json_parsed;
+	return ret;
+}
+
 json_value* GetApplicationSettings() {
     static json_value* ret = new json_value();
     static bool settings_fetched = false;
     if (settings_fetched)
         return ret;
-    settings_fetched = true;
-    LOG_DEBUG << "Fetching settings from settings.json file";
+	const std::string startConfFile = "start.conf";
+	std::string settingsFileName = startConfFile;
+	LOG_DEBUG << "Fetching settings from " << settingsFileName << " file";
+	json_value* jsonValue = new json_value();
+	jsonValue = GetJson(settingsFileName);
+	if ((*jsonValue).type == json_none) {
+		return ret;
+	}
+	std::string location = "";
+	const std::string subFolder = (*jsonValue)["SubFolder"];
+	do {
+		std::string settings = (*jsonValue)["Settings"];
+		std::string fileName;
+		if (settings.empty()) {
+			fileName = (*jsonValue)["FileName"];
+			if (fileName.empty()) {
+				g_applicationSettingsError = "Error: " + settingsFileName + " file has neither FileName nor Settings.";
+				LOG_WARNING << g_applicationSettingsError;
+				return ret;
+			}
+		}
+		else {
+			settings_fetched = true;
+			fileName = settings;
+		}
+		std::string path = (*jsonValue)["Path"];
+		if (!path.empty()) if (location.empty()) 
+			location = path;
+		else 
+			location = location + "/" + path;
+		if (!subFolder.empty()) fileName = subFolder + "/" + fileName;
+		if (!location.empty()) fileName = location + "/" + fileName;
+		settingsFileName = fileName;
+		LOG_DEBUG << "Fetching settings from " << settingsFileName << " file";
+		jsonValue = GetJson(settingsFileName);
+		if ((*jsonValue).type == json_none) {
+			return ret;
+		}
+	} while (!settings_fetched);
 
-    std::string settingsFile = GetExecutableDirectory() + "\\settings.json";
-    std::string contents = GetFileContents(settingsFile);
-    if (contents.empty()) {
-        LOG_WARNING << "Error while opening the settings.json file";
-        g_applicationSettingsError = "Error while opening the Settings file. ";
-        return ret;
-    }
-
-    json_settings settings;
-    memset(&settings, 0, sizeof(json_settings));
-    char error[256];
-    json_value* json_parsed = json_parse_ex(&settings, contents.c_str(),
-                                            &error[0]);
-    if (json_parsed == 0) {
-        LOG_WARNING << "Error while parsing settings.json file: " << error;
-        g_applicationSettingsError = "Error while parsing the Settings file. ";
-        return ret;
-    }
-    ret = json_parsed;
-    return ret;
+	ret = jsonValue;
+	return ret;
 }
 
 std::string GetApplicationSettingsError() {
